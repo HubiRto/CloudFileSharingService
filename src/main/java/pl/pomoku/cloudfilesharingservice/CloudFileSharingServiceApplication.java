863fcd8fc83d82916fd51e4198a534791ebeb976
hubirto <hubert.rybicki.hr@gmail.com>
2024-07-18 22:28:16 +0200

    Add all necessary dependencies

--- src/main/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplication.java
+++ src/main/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudFileSharingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudFileSharingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFileSharingServiceApplication.class, args);
+    }
 
 }
--- src/test/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplicationTests.java
+++ src/test/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CloudFileSharingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
