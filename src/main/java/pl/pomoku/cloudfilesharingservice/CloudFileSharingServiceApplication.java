33a32fa6b7c658cb109635ce958ce584eee25513
hubirto <hubert.rybicki.hr@gmail.com>
2024-07-18 22:26:22 +0200

    Initial commit

---
+++ src/main/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.pomoku.cloudfilesharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileSharingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileSharingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pomoku/cloudfilesharingservice/CloudFileSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomoku.cloudfilesharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
